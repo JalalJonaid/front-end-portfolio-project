dee8cf24f3d1c1cdd35d2ce132a752d0eb8513cc
Jalal Jonaid <jalaljonaid@Jalals-MacBook-Air.local>
2023-07-12 19:32:56 -0400

    Testing Code w/ Pokemon AI

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Test");
+
+function getPokemon() {
+    fetch('https://pokeapi.co/api/v2/type/3').then((response) => response.json()).then((data) => {
+        console.log(data);
+    }).catch((err) => {
+        console.log('Pokemon Type not Found', err);
+    })
+}
+
+getPokemon();
