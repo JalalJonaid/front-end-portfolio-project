445723a79ab123c1bbbe8cb7e8102d09b7346f96
Jalal Jonaid <jalaljonaid@Jalals-MacBook-Air.local>
2023-07-12 20:17:19 -0400

    Added basic functionality - on type id number enter returns type

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-console.log("Test");
+document.querySelector("#search").addEventListener("click", getPokemon);
 
-function getPokemon() {
-    fetch('https://pokeapi.co/api/v2/type/3').then((response) => response.json()).then((data) => {
-        console.log(data);
-    }).catch((err) => {
-        console.log('Pokemon Type not Found', err);
+function getPokemon(e) {
+  const type = document.querySelector("#pokemonType").value;
+
+  fetch(`https://pokeapi.co/api/v2/type/${type}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".pokemonBox").innerHTML = `
+        <div>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/22.png" alt="bird"/>
+        </div>
+        <div class="pokemonInfo">
+        <h1></h1>
+        <h2>Type: ${data.name}</h2>
+        </div>
+        `;
     })
-}
+    .catch((err) => {
+      console.log("Pokemon Type not Found", err);
+    });
 
-getPokemon();
+  e.preventDefault();
+}
